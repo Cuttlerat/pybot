9342c2e7b3a66e818543ef66c7285c6598a96ad6
Cuttlerat <avkioller@gmail.com>
2019-05-14 22:28:53 +0300

    Hotfix config

--- bot/config.py
+++ bot/config.py
@@ -39,7 +39,7 @@ class Config:
             self.__db_host) if self.__db_host is not None else None
         self.__engine = create_engine(self.__database) if self.__database is not None else None
 
-        self.__log_level = cfg['log']['level'] if 'level' in cfg['log'] else "INFO"
+        self.__log_level = cfg['log']['level'] if 'log' in cfg and 'level' in cfg['log'] else "INFO"
 
     def telegram_token(self):
         if self.__tg_token is None:
