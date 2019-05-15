622488ff108bcca52ad07070a841ae7832ce0996
Roman Zayrullin <krosenmann@gmail.com>
2019-05-15 08:59:48 +0300

    There will be only one!

--- bot/logger.py
+++ bot/logger.py
@@ -3,7 +3,16 @@ from dateutil.tz import tzlocal
 import json
 
 
-class Log:
+class Watchmen(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Watchmen, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Log(metaclass=Watchmen):
 
     levels ={"DEBUG": 0,
              "INFO": 1,
